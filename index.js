0ae3d00ea6c229d361f64afa09d8e41cc96cf3fc
antoine lanoe <antoine.lanoe@linkfluence.com>
2023-04-20 14:47:53 +0200

    fix: simplify index script

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
 "use strict";
-import { fileURLToPath } from "url";
+import { createProject } from "./tasks/create-project.js";
 import { dirname } from "path";
 import { displayIntroMessage } from "./tasks/intro-message.js";
-import { createProject } from "./tasks/create-project.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const CURRENT_DIRECTORY = process.cwd();
+import { fileURLToPath } from "url";
 
 displayIntroMessage();
-createProject(CURRENT_DIRECTORY, __dirname);
+createProject(process.cwd(), dirname(fileURLToPath(import.meta.url)));
